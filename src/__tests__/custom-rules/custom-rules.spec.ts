fac5f65c960bc0b1680ea5712efd9913af75511f
Wei Li <ng_wei_li@tech.gov.sg>
2023-02-02 15:14:53 +0800

    [FIELDS][WL] validate values against options in checkbox

--- src/__tests__/custom-rules/custom-rules.spec.ts
+++ src/__tests__/custom-rules/custom-rules.spec.ts
@@ -6,30 +6,51 @@ const ERROR_MESSAGE = "test error message";
 
 describe("custom-rules", () => {
 	it.each`
-		type        | condition                    | fieldType     | config                              | valid                 | invalid
-		${"string"} | ${"uinfin"}                  | ${"text"}     | ${{ uinfin: true }}                 | ${"S1234567D"}        | ${"S1234567A"}
-		${"string"} | ${"filled"}                  | ${"text"}     | ${{ filled: true }}                 | ${"hello"}            | ${undefined}
-		${"string"} | ${"empty"}                   | ${"text"}     | ${{ empty: true }}                  | ${undefined}          | ${"hello"}
-		${"string"} | ${"empty (empty string)"}    | ${"text"}     | ${{ empty: true }}                  | ${""}                 | ${"hello"}
-		${"string"} | ${"equals"}                  | ${"text"}     | ${{ equals: "hello" }}              | ${"hello"}            | ${"hi"}
-		${"string"} | ${"notEquals"}               | ${"text"}     | ${{ notEquals: "hello" }}           | ${"hi"}               | ${"hello"}
-		${"number"} | ${"filled"}                  | ${"numeric"}  | ${{ filled: true }}                 | ${1}                  | ${undefined}
-		${"number"} | ${"empty"}                   | ${"numeric"}  | ${{ empty: true }}                  | ${undefined}          | ${1}
-		${"number"} | ${"equals"}                  | ${"numeric"}  | ${{ equals: 1 }}                    | ${1}                  | ${2}
-		${"number"} | ${"notEquals"}               | ${"numeric"}  | ${{ notEquals: 1 }}                 | ${2}                  | ${1}
-		${"array"}  | ${"filled"}                  | ${"checkbox"} | ${{ filled: true }}                 | ${["hello"]}          | ${[]}
-		${"array"}  | ${"empty"}                   | ${"checkbox"} | ${{ empty: true }}                  | ${[]}                 | ${["hello"]}
-		${"array"}  | ${"equals"}                  | ${"checkbox"} | ${{ equals: ["hello"] }}            | ${["hello"]}          | ${["hi"]}
-		${"array"}  | ${"notEquals"}               | ${"checkbox"} | ${{ notEquals: ["hello"] }}         | ${["hi"]}             | ${["hello"]}
-		${"array"}  | ${"notEquals (empty array)"} | ${"checkbox"} | ${{ notEquals: [] }}                | ${["hi"]}             | ${[]}
-		${"array"}  | ${"includes string"}         | ${"checkbox"} | ${{ includes: "hello" }}            | ${["hello"]}          | ${["hi"]}
-		${"array"}  | ${"excludes string"}         | ${"checkbox"} | ${{ excludes: "hello" }}            | ${["hi"]}             | ${["hello"]}
-		${"array"}  | ${"includes array"}          | ${"checkbox"} | ${{ includes: ["hello", "world"] }} | ${["hello", "world"]} | ${["hi"]}
-		${"array"}  | ${"excludes array"}          | ${"checkbox"} | ${{ excludes: ["hello", "world"] }} | ${["hi"]}             | ${["hello", "hi", "world"]}
+		type        | condition                 | fieldType    | config                    | valid          | invalid
+		${"string"} | ${"uinfin"}               | ${"text"}    | ${{ uinfin: true }}       | ${"S1234567D"} | ${"S1234567A"}
+		${"string"} | ${"filled"}               | ${"text"}    | ${{ filled: true }}       | ${"hello"}     | ${undefined}
+		${"string"} | ${"empty"}                | ${"text"}    | ${{ empty: true }}        | ${undefined}   | ${"hello"}
+		${"string"} | ${"empty (empty string)"} | ${"text"}    | ${{ empty: true }}        | ${""}          | ${"hello"}
+		${"string"} | ${"equals"}               | ${"text"}    | ${{ equals: "hello" }}    | ${"hello"}     | ${"hi"}
+		${"string"} | ${"notEquals"}            | ${"text"}    | ${{ notEquals: "hello" }} | ${"hi"}        | ${"hello"}
+		${"number"} | ${"filled"}               | ${"numeric"} | ${{ filled: true }}       | ${1}           | ${undefined}
+		${"number"} | ${"empty"}                | ${"numeric"} | ${{ empty: true }}        | ${undefined}   | ${1}
+		${"number"} | ${"equals"}               | ${"numeric"} | ${{ equals: 1 }}          | ${1}           | ${2}
+		${"number"} | ${"notEquals"}            | ${"numeric"} | ${{ notEquals: 1 }}       | ${2}           | ${1}
 	`("should support $condition condition for Yup $type type", ({ fieldType, config, valid, invalid }) => {
 		const schema = jsonToSchema({
 			field: {
 				fieldType,
+				validation: [{ ...config, errorMessage: ERROR_MESSAGE }],
+			},
+		});
+		expect(() => schema.validateSync({ field: valid })).not.toThrowError();
+		expect(TestHelper.getError(() => schema.validateSync({ field: invalid }, { abortEarly: false })).message).toBe(
+			ERROR_MESSAGE
+		);
+	});
+
+	it.each`
+		type       | condition                    | config                              | valid                           | invalid
+		${"array"} | ${"filled"}                  | ${{ filled: true }}                 | ${["Apple"]}                    | ${[]}
+		${"array"} | ${"empty"}                   | ${{ empty: true }}                  | ${[]}                           | ${["Apple"]}
+		${"array"} | ${"equals"}                  | ${{ equals: ["Apple"] }}            | ${["Apple"]}                    | ${["Berry"]}
+		${"array"} | ${"notEquals"}               | ${{ notEquals: ["Apple"] }}         | ${["Berry"]}                    | ${["Apple"]}
+		${"array"} | ${"notEquals (empty array)"} | ${{ notEquals: [] }}                | ${["Apple"]}                    | ${[]}
+		${"array"} | ${"includes string"}         | ${{ includes: "Apple" }}            | ${["Apple", "Durian"]}          | ${["Berry"]}
+		${"array"} | ${"excludes string"}         | ${{ excludes: "Apple" }}            | ${["Berry", "Durian"]}          | ${["Apple"]}
+		${"array"} | ${"includes array"}          | ${{ includes: ["Apple", "Berry"] }} | ${["Apple", "Berry", "Durian"]} | ${["Cherry"]}
+		${"array"} | ${"excludes array"}          | ${{ excludes: ["Apple", "Berry"] }} | ${["Cherry"]}                   | ${["Apple", "Berry", "Durian"]}
+	`("should support $condition condition for Yup $type type", ({ config, valid, invalid }) => {
+		const schema = jsonToSchema({
+			field: {
+				fieldType: "checkbox",
+				options: [
+					{ label: "Apple", value: "Apple" },
+					{ label: "Berry", value: "Berry" },
+					{ label: "Cherry", value: "Cherry" },
+					{ label: "Durian", value: "Durian" },
+				],
 				validation: [{ ...config, errorMessage: ERROR_MESSAGE }],
 			},
 		});
--- src/fields/checkbox.ts
+++ src/fields/checkbox.ts
@@ -3,9 +3,15 @@ import { IFieldSchemaBase } from "../schema-generator";
 import { ERROR_MESSAGES } from "../shared";
 import { IFieldGenerator } from "./types";
 
-export interface ICheckboxSchema<V = undefined> extends IFieldSchemaBase<"checkbox", V> {}
+interface IOption {
+	label: string;
+	value: string;
+}
+export interface ICheckboxSchema<V = undefined> extends IFieldSchemaBase<"checkbox", V> {
+	options: IOption[];
+}
 
-export const checkbox: IFieldGenerator<ICheckboxSchema> = (id, { validation }) => {
+export const checkbox: IFieldGenerator<ICheckboxSchema> = (id, { options, validation }) => {
 	const isRequiredRule = validation?.find((rule) => "required" in rule);
 
 	return {
@@ -15,12 +21,20 @@ export const checkbox: IFieldGenerator<ICheckboxSchema> = (id, { validation }) =
 				.test(
 					"is-empty-array",
 					isRequiredRule?.errorMessage || ERROR_MESSAGES.COMMON.REQUIRED_OPTION,
-					(value) => {
-						if (!value || !isRequiredRule?.required) return true;
+					(values) => {
+						if (!values || !isRequiredRule?.required) return true;
 
-						return value.length > 0;
+						return values.length > 0;
 					}
-				),
+				)
+				.test("validate-options", ERROR_MESSAGES.COMMON.INVALID_OPTION, (values) => {
+					if (!values || !values.length) return true;
+
+					return (
+						values.filter((value) => options.find((option) => option.value === value)).length ===
+						values.length
+					);
+				}),
 			validation,
 		},
 	};

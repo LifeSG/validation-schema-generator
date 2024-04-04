009f481570616dec668827c67bbea10baaf2db21
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2024-04-04 13:39:07 +0800

    Built package | Version: v1.1.1 | Gitlab: validation-schema-generator main 3637279

--- src/utils/image-helper.ts
+++
@@ -1,8 +0,0 @@
-import sizeOf from "image-size";
-
-export namespace ImageHelper {
-	export const getDimensionsFromBase64 = (base64: string) => {
-		const image = Buffer.from(base64.split(";base64,").pop(), "base64");
-		return sizeOf(image);
-	};
-}
--- src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./file-helper";
-export * from "./image-helper";
-export * from "./object-helper";
-export * from "./test-helper";
-export * from "./value-helper";

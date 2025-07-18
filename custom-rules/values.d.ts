cf4e0a6cd9cda7805962a51de706976aa80f9259
group_7001_bot_1 <group_7001_bot_1@noreply.sgts.gitlab-dedicated.com>
2025-07-18 16:10:41 +0800

    Built package | Version: v1.3.6 | Gitlab: validation-schema-generator main 11444723

---
+++ custom-rules/values.d.ts
@@ -0,0 +1,7 @@
+export declare const filled: () => void;
+export declare const empty: () => void;
+export declare const equals: () => void;
+export declare const notEquals: () => void;
+export declare const notMatches: () => void;
+export declare const withinDays: () => void;
+export declare const beyondDays: () => void;

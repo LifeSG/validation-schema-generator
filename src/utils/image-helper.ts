2961899d5e9bb12707218a8eb7dc0d429de9eb92
Wei Li <ng_wei_li@tech.gov.sg>
2026-09-14 15:33:56 +0800

    [MISC][WL] add unit tests for image-helper

--- src/utils/image-helper.ts
+++ src/utils/image-helper.ts
@@ -4,7 +4,7 @@ interface IImageDimensions {
 }
 
 // reference: https://github.com/sindresorhus/is-png/blob/main/index.js
-const PNG_SIGNATURE = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
+export const PNG_SIGNATURE = [0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a];
 const isPng = (buffer: Buffer): boolean =>
 	buffer.length >= 8 && PNG_SIGNATURE.every((byte, index) => buffer[index] === byte);
 
